75673a6f97e4bd6859791bb5ed7e2a88e78da119
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-02 12:09:24 +0900

    [Update] IntroActivity 추가

--- app/src/main/java/kr/hs/dgsw/trust/activity/IntroActivity.kt
+++ app/src/main/java/kr/hs/dgsw/trust/activity/IntroActivity.kt
@@ -1,11 +1,12 @@
-package kr.hs.dgsw.trust
+package kr.hs.dgsw.trust.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.hs.dgsw.trust.R
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
---
+++ app/src/main/java/kr/hs/dgsw/trust/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.trust.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.trust.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
