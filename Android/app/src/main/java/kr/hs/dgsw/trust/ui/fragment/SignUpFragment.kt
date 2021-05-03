451ae7b4c9a589aa726610548ad57fa155ce14cd
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-04 08:43:38 +0900

    [Update] SignUpFragment 추가

--- Android/app/src/main/java/kr/hs/dgsw/trust/ui/fragment/SignUpFragment.kt
+++ Android/app/src/main/java/kr/hs/dgsw/trust/ui/fragment/SignUpFragment.kt
@@ -1,23 +1,44 @@
 package kr.hs.dgsw.trust.ui.fragment
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import kr.hs.dgsw.trust.R
+import kr.hs.dgsw.trust.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+
+    private val toolbar : Toolbar by lazy {
+        binding.toolbar
+    }
+
+    private val navController : NavController by lazy {
+        findNavController()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        NavigationUI.setupWithNavController(toolbar, navController)
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_close_24)
+
     }
 }
