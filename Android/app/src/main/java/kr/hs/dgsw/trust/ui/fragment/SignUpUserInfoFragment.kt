39a129a30b15df8b414f1164a4f311a5cd1ab1f8
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-05 14:31:44 +0900

    [Update] 회원가입 화면 이동 추가

--- Android/app/src/main/java/kr/hs/dgsw/trust/ui/fragment/SignUpUserInfoFragment.kt
+++ Android/app/src/main/java/kr/hs/dgsw/trust/ui/fragment/SignUpUserInfoFragment.kt
@@ -5,19 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import kr.hs.dgsw.trust.R
+import kr.hs.dgsw.trust.databinding.FragmentSignUpUserInfoBinding
+import kr.hs.dgsw.trust.viewmodel.fragment.SignUpViewModel
 
 class SignUpUserInfoFragment : Fragment() {
+
+    private lateinit var viewModel : SignUpViewModel
+
+    private lateinit var binding : FragmentSignUpUserInfoBinding
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
+
+    private lateinit var btnSignUp : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_user_info, container, false)
+    ): View {
+        viewModel = ViewModelProvider(requireActivity())[SignUpViewModel::class.java]
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up_user_info, container, false)
+
+        binding.vm = viewModel
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+        btnSignUp.setOnClickListener {
+            viewModel.signUp()
+            navController.navigate(R.id.action_signUpUserInfoFragment_to_signUpSuccessFragment)
+        }
+    }
+
+    private fun init() {
+        btnSignUp = binding.btnSignUpSignUp
     }
 }
