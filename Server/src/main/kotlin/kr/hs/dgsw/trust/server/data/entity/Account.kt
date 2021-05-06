21dc2ca9bec9281762ec797ad15b98aa10d1ad8f
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-06 14:37:31 +0900

    [Update] Account Entity 추가

---
+++ Server/src/main/kotlin/kr/hs/dgsw/trust/server/data/entity/Account.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.trust.server.data.entity
+
+import javax.persistence.*
+
+@Entity(name = "account")
+class Account {
+    @Column(name = "name_")
+    var name: String? = null
+    @Id
+    var username: String? = null
+    var password: String? = null
+}
