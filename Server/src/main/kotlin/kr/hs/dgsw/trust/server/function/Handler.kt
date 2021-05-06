2d0ec985d5ffd87fc629eccde4f5e5bc9587a68c
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-06 14:38:14 +0900

    [Update] Exception Handler Function 추가

---
+++ Server/src/main/kotlin/kr/hs/dgsw/trust/server/function/Handler.kt
@@ -0,0 +1,9 @@
+package kr.hs.dgsw.trust.server.function
+
+import kr.hs.dgsw.trust.server.data.response.JsonResponse
+import kr.hs.dgsw.trust.server.exception.BadRequestException
+import kr.hs.dgsw.trust.server.exception.UnauthenticatedException
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+
