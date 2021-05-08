459e267a9c7ff213535c0fb47c8c009ac8e2ad63
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-08 13:20:38 +0900

    [Update] 스프링 Bean으로 PasswordEncoder 객체 생성 및 AccountController에 주입

---
+++ Server/src/main/kotlin/kr/hs/dgsw/trust/server/configuration/AppConfig.kt
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.trust.server.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class AppConfig {
+
+    @Bean
+    fun passwordEncoder() : PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+}
--- Server/src/main/kotlin/kr/hs/dgsw/trust/server/controller/AccountController.kt
+++ Server/src/main/kotlin/kr/hs/dgsw/trust/server/controller/AccountController.kt
@@ -6,11 +6,17 @@ import kr.hs.dgsw.trust.server.exception.BadRequestException
 import kr.hs.dgsw.trust.server.exception.UnauthenticatedException
 import kr.hs.dgsw.trust.server.repository.AccountRepository
 import org.springframework.http.HttpStatus
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/account")
-class AccountController(private val accountRepository: AccountRepository) {
+class AccountController(private val accountRepository: AccountRepository,
+                        private val passwordEncoder : PasswordEncoder
+                        ) {
+
+
     @PostMapping("/login")
     fun login(username: String, password: String) : HashMap<String, Any?> {
         val account = Account()
@@ -19,8 +25,9 @@ class AccountController(private val accountRepository: AccountRepository) {
 
         return if (isIdAndPwNotNull(account)) {
             if (isIdAndPwExist(account)) {
-                //TODO 토큰
-                JsonResponse().returnResponse("200", "로그인에 성공하였습니다.", null)
+                account.name = getName(account)
+                account.password = ""
+                JsonResponse().returnResponse("200", "로그인에 성공하였습니다.", account)
             } else {
                 throw UnauthenticatedException("아이디 또는 비밀번호가 잘못되었습니다.")
             }
@@ -29,12 +36,26 @@ class AccountController(private val accountRepository: AccountRepository) {
         }
     }
 
+    fun getName(account: Account) : String {
+        val account = accountRepository.findById(account.username!!).orElseThrow()
+        return account.name!!
+    }
+
     fun isIdAndPwExist(account: Account) : Boolean {
         return try {
-            accountRepository.findById(account.username!!).get()
-            true
+            val foundAccount = accountRepository.findById(account.username!!).get()
+            passwordEncoder.matches(account.password!!, foundAccount.password)
         } catch (e: Exception) {
             false
+        }
+    }
+
+    fun isNotIdExist(account: Account) : Boolean {
+        return try {
+            accountRepository.findById(account.username!!).get()
+            false
+        } catch (e: Exception) {
+            true
         }
     }
 
@@ -49,12 +70,13 @@ class AccountController(private val accountRepository: AccountRepository) {
         val account = Account()
         account.name = name
         account.username = username
-        account.password = password
+        account.password = passwordEncoder.encode(password)
 
         return if (isAccountInfoNotNull(account)) {
-            if (!isIdAndPwExist(account)) {
+            if (isNotIdExist(account)) {
                 accountRepository.save(account)
-                JsonResponse().returnResponse("200", "회원가입에 성공하였습니다.", null)
+                account.password = ""
+                JsonResponse().returnResponse("200", "회원가입에 성공하였습니다.", account)
             } else {
                 throw UnauthenticatedException("중복된 아이디가 있습니다.")
             }
