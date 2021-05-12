0032027a7a14603608ef9f289d57b4f018b58786
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-12 16:08:50 +0900

    [Update] MainActivity에 Fragment(Navigation), BottomNavigationView 수정

--- Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/activity/MainActivity.kt
+++ Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/activity/MainActivity.kt
@@ -1,12 +1,26 @@
 package kr.hs.dgsw.trust.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kr.hs.dgsw.trust.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    private val navController: NavController by lazy {
+        findNavController(R.id.navHost_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bnv_main)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
---
+++ Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/fragment/BoardFragment.kt
@@ -0,0 +1,44 @@
+package kr.hs.dgsw.trust.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import kr.hs.dgsw.trust.R
+
+class BoardFragment : Fragment() {
+
+    private lateinit var toolbar: Toolbar
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_board, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbar = view.findViewById(R.id.toolbar_board)
+
+        val appbarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.boardFragment,
+                R.id.chatFragment,
+                R.id.userInfoFragment
+            ), null
+        )
+        NavigationUI.setupWithNavController(toolbar, navController, appbarConfiguration)
+    }
+}
