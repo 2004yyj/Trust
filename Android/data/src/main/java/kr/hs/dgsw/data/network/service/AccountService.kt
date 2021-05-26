102ae54c3b821f12c15d59cfb42bf3aa6c247f26
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-26 16:32:58 +0900

    [Fixed]

--- Android/data/src/main/java/kr/hs/dgsw/data/network/service/AccountService.kt
+++ Android/data/src/main/java/kr/hs/dgsw/data/network/service/AccountService.kt
@@ -6,7 +6,6 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.*
-import kotlin.reflect.typeOf
 
 interface AccountService {
     @POST("/account/login")
