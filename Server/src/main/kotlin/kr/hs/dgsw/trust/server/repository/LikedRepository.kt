07b42d17df2c4aa62d1847af008af0ab9796cd2b
양윤재 <diddbswo04@dgsw.hs.kr>
2021-05-31 13:49:51 +0900

    [Update] 좋아요 레포지토리 인터페이스에 username, postid를 통한 좋아요 쿼리 기능 추가

--- Server/src/main/kotlin/kr/hs/dgsw/trust/server/repository/LikedRepository.kt
+++ Server/src/main/kotlin/kr/hs/dgsw/trust/server/repository/LikedRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface LikedRepository : JpaRepository<Liked, Int?> {
-    fun findByPostId(postId: Int) : List<Liked>
+    fun findAllByPostId(postId: Int) : List<Liked>
     fun deleteAllByPostId(postId: Int)
+    fun existsByUsernameAndPostId(username: String, postId: Int) : Boolean
 }
