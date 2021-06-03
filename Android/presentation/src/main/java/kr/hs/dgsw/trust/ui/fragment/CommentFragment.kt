606b80e56f71bf8dd8ab40b8733d8e192e7092dc
양윤재 <diddbswo04@dgsw.hs.kr>
2021-06-03 11:33:03 +0900

    [Update] 댓글 창 다이얼로그 MotionLayout 추가

--- Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/fragment/CommentFragment.kt
+++ Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/fragment/CommentFragment.kt
@@ -1,9 +1,11 @@
 package kr.hs.dgsw.trust.ui.fragment
 
 import android.os.Bundle
+import android.transition.Transition
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import kr.hs.dgsw.trust.R
@@ -12,6 +14,12 @@ import kr.hs.dgsw.trust.ui.viewmodel.fragment.CommentViewModel
 class CommentFragment : DialogFragment() {
 
     private lateinit var viewModel: CommentViewModel
+    private lateinit var motionLayout: MotionLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.FullScreenDialog)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +34,22 @@ class CommentFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         observe()
+
+        motionLayout = view.findViewById(R.id.motionLayout_comment)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                this@CommentFragment.dismiss()
+            }
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
         viewModel.postId.value = requireArguments().getInt("postId")
     }
