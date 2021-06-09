1767e710fa84ed85143baedaec63cd0f323ab3f9
양윤재 <diddbswo04@dgsw.hs.kr>
2021-06-09 17:38:53 +0900

    [Update] CommentFragment에 데이터 바인딩 추가/TransitionListener 익스텐션 사용

--- Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/dialog/CommentFragment.kt
+++ Android/presentation/src/main/java/kr/hs/dgsw/trust/ui/dialog/CommentFragment.kt
@@ -7,15 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import kr.hs.dgsw.domain.usecase.comment.GetAllCommentUseCase
 import kr.hs.dgsw.trust.R
+import kr.hs.dgsw.trust.databinding.FragmentCommentBinding
+import kr.hs.dgsw.trust.di.application.MyDaggerApplication
+import kr.hs.dgsw.trust.ui.adapter.CommentAdapter
+import kr.hs.dgsw.trust.ui.util.setOnTransitionCompletedListener
+import kr.hs.dgsw.trust.ui.viewmodel.factory.CommentViewModelFactory
 import kr.hs.dgsw.trust.ui.viewmodel.fragment.CommentViewModel
+import javax.inject.Inject
 
 class CommentFragment : DialogFragment() {
 
+    @Inject
+    lateinit var getAllCommentUseCase: GetAllCommentUseCase
+
     private lateinit var viewModel: CommentViewModel
     private lateinit var motionLayout: MotionLayout
+    private lateinit var binding: FragmentCommentBinding
+    private val recyclerAdapter: CommentAdapter by lazy { CommentAdapter() }
+    private val recyclerView: RecyclerView by lazy { binding.rvCommentListComment }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,41 +40,41 @@ class CommentFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
 
-        viewModel = ViewModelProvider(this)[CommentViewModel::class.java]
+        (requireActivity().application as MyDaggerApplication).daggerComponent.inject(this)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_comment, container, false)
+        viewModel = ViewModelProvider(this, CommentViewModelFactory(getAllCommentUseCase))[CommentViewModel::class.java]
+        viewModel.postId.value = requireArguments().getInt("postId")
 
-        return inflater.inflate(R.layout.fragment_comment, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
         observe()
 
         motionLayout = view.findViewById(R.id.motionLayout_comment)
-        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                val handler = Handler(Looper.getMainLooper())
-                handler.postDelayed({
-                            dismiss()
-                }, 10)
-            }
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-            }
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-            }
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
-            }
+        motionLayout.setOnTransitionCompletedListener { _, _ ->
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                dismiss()
+            }, 10)
+        }
+    }
 
-        })
-
-        viewModel.postId.value = requireArguments().getInt("postId")
+    private fun init() {
+        recyclerView.adapter = recyclerAdapter
     }
 
     private fun observe() {
         viewModel.postId.observe(this) {
-            viewModel.comment(it)
+            viewModel.getAllComment(it)
+        }
+        viewModel.commentList.observe(this) {
+            recyclerAdapter.submitList(it)
         }
     }
 
