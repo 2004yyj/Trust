d3bbd7a28e97e6bdab5877bf2b1c456ff0e7bcf6
양윤재 <diddbswo04@dgsw.hs.kr>
2021-06-12 23:18:47 +0900

    [Update] AuthInterceptor을 추가하여 요청을 가로챌 수 있도록 함

--- Android/presentation/src/main/java/kr/hs/dgsw/trust/di/module/RetrofitModule.kt
+++ Android/presentation/src/main/java/kr/hs/dgsw/trust/di/module/RetrofitModule.kt
@@ -3,7 +3,9 @@ package kr.hs.dgsw.trust.di.module
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
-import kr.hs.dgsw.trust.ui.util.ServerAddress.ADDR
+import kr.hs.dgsw.data.util.ServerAddress.ADDR
+import kr.hs.dgsw.trust.interceptor.AuthInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,13 +16,26 @@ class RetrofitModule {
 
     @Provides
     @Singleton
-    fun gson() = Gson()
+    fun proviceInterceptor() = AuthInterceptor()
 
     @Provides
     @Singleton
-    fun provideRetrofit(gson: Gson): Retrofit = Retrofit.Builder()
-        .baseUrl(ADDR)
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build()
+    fun provideClient(interceptor: AuthInterceptor) =
+        OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun proviceGson() = Gson()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson, client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(client)
+            .baseUrl(ADDR)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
 }
---
+++ Android/presentation/src/main/java/kr/hs/dgsw/trust/interceptor/AuthInterceptor.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.trust.interceptor
+
+import kr.hs.dgsw.trust.ui.util.PreferenceHelper.token
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
+        return chain.proceed(request)
+    }
+}
